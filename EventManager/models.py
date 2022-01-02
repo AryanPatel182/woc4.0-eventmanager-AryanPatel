3a00f2d8bf80388614bf09f2978cf152450ba6d1
AryanPatel <asp6304@gmail.com>
2022-01-02 22:12:48 +0530

    Two forms done

--- EventManager/models.py
+++ EventManager/models.py
@@ -1,4 +1,21 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    desc = models.CharField(max_length=200)
+    poster_link = models.URLField( max_length=200)
+    from_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    to_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    deadline = models.DateTimeField(auto_now=False, auto_now_add=False)
+    host_email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=32, default="")
 
+class Participant(models.Model):
+    participant_name = models.CharField(max_length=30,default="")
+    contact_no = models.BigIntegerField()    
+    participant_email = models.EmailField(max_length=254)
+    event_name = models.ForeignKey(Event, on_delete=models.CASCADE)
+    registration_type = models.CharField(max_length=20)
+    no_of_people = models.IntegerField(default=1)
--- EventManager/views.py
+++ EventManager/views.py
@@ -1,15 +1,48 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from EventManager.models import Event, Participant
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')    
 
 def event_registration(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        desc = request.POST.get('desc')
+        poster_link = request.POST.get('poster_link')
+        from_date = request.POST.get('from_date')
+        to_date = request.POST.get('to_date')
+        deadline = request.POST.get('deadline')
+        host_email = request.POST.get('host_email')
+        password = request.POST.get('password')
+
+        event = Event(name=name, desc=desc, poster_link=poster_link, from_date=from_date, to_date=to_date, deadline=deadline, host_email=host_email, password=password)        
+        event.save()
+
+        return render(request, 'index.html')
     return render(request, 'event_form.html')
 
 def participant_registration(request):
-    return render(request, 'participation_form.html')
+    if request.method == 'POST':
+        participant_name = request.POST.get('participant_name')
+        contact_no = request.POST.get('contact_no')
+        participant_email = request.POST.get('participant_email')
+        event_name = request.POST.get('event_name')
+        registration_type = request.POST.get('registration_type')
+        no_of_people = request.POST.get('no_of_people')    
+        evnt = Event.objects.get(name=event_name)   
+
+        participant = Participant(participant_name=participant_name, contact_no=contact_no, participant_email=participant_email, event_name=evnt, registration_type=registration_type, no_of_people=no_of_people)
+
+        participant.save()
+        # print(participant)
+        return render(request, 'index.html')
+
+    context = {'event_list': Event.objects.all()}
+    return render(request, 'participation_form.html', context)
 
 def event_dashboard(request):
     return render(request, 'event_dashboard.html')
+

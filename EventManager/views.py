9bf4a02e5c4efc4f77a2489a7ad6830e22a0d7d6
AryanPatel <asp6304@gmail.com>
2022-01-03 19:38:19 +0530

    mail service added

--- EventManager/views.py
+++ EventManager/views.py
@@ -1,8 +1,13 @@
+from django.core.checks import messages
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.core.mail import send_mail
 from EventManager.models import Event, Participant
+from datetime import datetime
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Create your views here.
 def index(request):
     return render(request, 'index.html')    
@@ -19,8 +24,14 @@ def event_registration(request):
         password = request.POST.get('password')
 
         event = Event(name=name, desc=desc, poster_link=poster_link, from_date=from_date, to_date=to_date, deadline=deadline, host_email=host_email, password=password)        
-        event.save()
-
+        # event.save()
+        send_mail(
+           'Your event :',
+            'Here is the confirmation message.\n Good Luck !',
+            os.environ.get("EMAIL_HOST_USER"),
+            [host_email],
+            fail_silently=False,
+        )
         return render(request, 'index.html')
     return render(request, 'event_form.html')
 
@@ -32,17 +43,33 @@ def participant_registration(request):
         event_name = request.POST.get('event_name')
         registration_type = request.POST.get('registration_type')
         no_of_people = request.POST.get('no_of_people')    
-        evnt = Event.objects.get(name=event_name)   
+        evnt = Event.objects.all().filter(name=event_name).first()   
 
+        # Already Registered
+        if Participant.objects.all().filter(participant_name=participant_name, contact_no=contact_no,participant_email=participant_email,event_name=evnt):
+            context = {'event_list': Event.objects.all(),'messages':"You have already participated in the event !"}
+            return render(request, 'participation_form.html', context)
+
+        # New participant
         participant = Participant(participant_name=participant_name, contact_no=contact_no, participant_email=participant_email, event_name=evnt, registration_type=registration_type, no_of_people=no_of_people)
 
-        participant.save()
-        # print(participant)
-        return render(request, 'index.html')
+        # participant.save()
+        # print(participant)        
 
-    context = {'event_list': Event.objects.all()}
+    context = {'event_list': Event.objects.all().filter(deadline__gte=datetime.now())}
     return render(request, 'participation_form.html', context)
 
 def event_dashboard(request):
+    if request.method == 'POST':
+        event_id = request.POST.get('event_id')
+        password = request.POST.get('password')
+
+        
+        if  Event.objects.all().filter(id=event_id).exists() and Event.objects.get(id=event_id).password == password:
+            context = {'participant_list': Participant.objects.all().filter(event_name=event_id)}
+            return render(request, 'event_dashboard.html',context) 
+        else:
+            context = {'messages':"Username or password is incorrect !"}
+            return render(request, 'event_dashboard.html', context)
     return render(request, 'event_dashboard.html')
 
